6b5c4d9e866d087ba64928f69303ddc4985be1e2
Elisia Morgan <33482645+elisiag10@users.noreply.github.com>
2022-02-05 15:55:52 -0500

    Update Rental.java

--- Rental.java
+++ Rental.java
@@ -1,59 +1,40 @@
-
-
 public class Rental {
-
-  //Two public final static fields that hold the number of minutes in an hour and the hourly rental rate 
+    // Two public final static fields that hold the number of minutes in an hour and
+    // the hourly rental rate
     final static int MINUTES_IN_AN_HOUR = 60;
     final static int RENT_PER_HOUR = 40;
 
-   
     private String contractNumber;
-    private  int hoursRented;
-    private  int additionalMinutes;
-    private  int total;
-    
-    
-    //Two public set methods
-     public void setContractNumber (String conNumber)
-     {
+    private int hoursRented;
+    private int additionalMinutes;
+    private int total;
 
-      String contractNumber = conNumber; 
+    // Two public set methods
+    public void setContractNumber(String x) {
+        contractNumber = x;
     }
 
-     public void setHoursAndMinutes(int x)
-      {
+    public void setHoursAndMinutes(int x) {
 
-          int hoursRented = x/MINUTES_IN_AN_HOUR;
-          int additionalMinutes = x % MINUTES_IN_AN_HOUR; 
-          int total = hoursRented * RENT_PER_HOUR + additionalMinutes; 
-      }
+        int user_input_time = x / MINUTES_IN_AN_HOUR;
+        additionalMinutes = x % MINUTES_IN_AN_HOUR;
+        total = user_input_time * RENT_PER_HOUR + additionalMinutes;
+    }
 
+    // Four public get methods that return the values in the four nonstatic fields.
+    public String getcontractNumber() {
+        return contractNumber;
+    }
 
-    //Four public get methods that return the values in the four nonstatic fields.
-     public String getcontractNumber()
-      {
+    public int gethoursRented() {
+        return hoursRented;
+    }
 
-          return contractNumber; 
-       
-      }
-
-     public int gethoursRented()
-      {
-
-        return hoursRented; 
-      }
-
-
-     public int getAdditionalMinutes()
-      {
-
+    public int getAdditionalMinutes() {
         return additionalMinutes;
-      }
+    }
 
-     public  int getTotal()
-     {
-       return total;
-      }
-
-
+    public int getTotal() {
+        return total;
+    }
 }
